6df7259fbc91ba553cc6470f7daa0d9ec4544959
unknown <peluffojuancruz@gmail.com>
2022-10-03 11:35:48 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+const app = require('./src/server/express');
+const { sequelize } = require('./src/models');
+
+const PORT = process.env.PORT || 4000;
+
+const start = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+    await sequelize.sync({ force: false });
+    console.log('All models were synchronized successfully.');
+    app.listen(PORT, console.log(`Listening on ${PORT}`));
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+start();
---
+++ src/config/sequelize.js
@@ -0,0 +1,29 @@
+module.exports = {
+  development: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+    logging: false,
+    define: {
+      timestamps: true,
+      underscored: true,
+    },
+  },
+  production: {
+    use_env_variable: 'DATABASE_URL',
+    dialect: process.env.DB_DIALECT,
+    logging: false,
+    define: {
+      timestamps: true,
+      underscored: true,
+    },
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  },
+};
